b1ed40485b1863a98dc7d22dedc695748a49f189
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-03-21 01:24:42 +0200

    ➕ Add: C#

---
+++ loop.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Loop {
+    class Loop {
+        static void Main(String[] args) {
+            for (int i;; i++) {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
