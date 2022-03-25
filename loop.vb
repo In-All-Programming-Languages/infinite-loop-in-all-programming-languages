477c4f5f3e0c10ce52f0a3bbdf6203d17df27347
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-03-25 22:26:10 +0200

    ➕ Add: Visual Basic loop

---
+++ loop.vb
@@ -0,0 +1,12 @@
+Imports System
+
+Public Class Test
+    Public Shared Sub Main()
+        Dim i as Integer = 0
+        
+        while (true)
+            System.Console.WriteLine(i)
+            i += 1
+        end while
+    End Sub
+End Class
